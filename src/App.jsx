c10fa3ff37a725a1a6b13b272b5994595f4ed7f0
hoangtuyen04work <164497260+hoangtuyen04work@users.noreply.github.com>
2025-03-10 11:35:38 +0700

    complete profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 // import ProtectedRoute from './components/ProtectedRoute'; // Import ProtectedRoute
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <main style={{ width: "100%", position: "fixed", top: 0 }}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </main>
   </Router>
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react"
+import { Navigate, NavLink } from "react-router-dom";
+import { doLogout } from "../redux/action/userAction";
+import { useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux";
+
+import "../styles/Home.scss"
+import LeftBar from "../components/LeftBar";
+import RightBar from "../components/RightBar";
+import CenterProfile from "../components/CenterProfile";
+
+function Home() {
+  return (
+    <div className="home">
+      <LeftBar />
+      <main className="home__main">
+        <CenterProfile/>
+      </main>
+      <RightBar />
+    </div>
+  );
+}
+
+export default Home;
